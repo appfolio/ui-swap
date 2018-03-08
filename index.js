d527b6ed97888acac1b327e67a94b72bc9274259
Aaron Panchal <aaron.panchal@appfolio.com>
2018-03-08 12:46:19 -0800

    extract version from query string and provide updated search

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-export default {
-  test: 'stuff'
-};
+export function extractVersion(search = '') {
+  const result = search.match(/(\?|&)ui_version=(.+?)(&|$)/);
+
+  return {
+    search: result ? search.replace(result[0], result[3] && result[1]) : search,
+    version: result && result[2]
+  };
+}
+
+export default function UISwap({ base, devBase }) {}
--- test.js
+++ test.js
@@ -1,7 +1,43 @@
 import test from 'ava';
 
-import UISwap from '.';
+import UISwap, { extractVersion } from '.';
 
-test('should have test key with stuff', t => {
-  t.is(UISwap.test, 'stuff');
+test('should get version from query string', t => {
+  t.falsy(extractVersion().version);
+  t.falsy(extractVersion('').version);
+  t.falsy(extractVersion('bullshit').version);
+  t.is(extractVersion('?ui_version=foobar').version, 'foobar');
+  t.is(extractVersion('?ui_version=foobar&other=param').version, 'foobar');
+  t.is(
+    extractVersion('?first=param&ui_version=foobar&other=param').version,
+    'foobar'
+  );
+  t.is(extractVersion('?first=param&ui_version=foobar').version, 'foobar');
+  t.falsy(extractVersion('?first=param&xui_version=foobar').version);
+  t.falsy(extractVersion('?first=param&ui_version=').version);
+});
+
+test('should extract version from query string', t => {
+  t.is(extractVersion().search, '');
+  t.is(extractVersion('').search, '');
+  t.is(extractVersion('bullshit').search, 'bullshit');
+  t.is(extractVersion('?ui_version=foobar').search, '');
+  t.is(extractVersion('?ui_version=foobar&other=param').search, '?other=param');
+  t.is(
+    extractVersion('?first=param&ui_version=foobar&other=param').search,
+    '?first=param&other=param'
+  );
+  t.is(extractVersion('?first=param&ui_version=foobar').search, '?first=param');
+  t.is(
+    extractVersion('?first=param&xui_version=foobar').search,
+    '?first=param&xui_version=foobar'
+  );
+  t.is(
+    extractVersion('?first=param&ui_version=').search,
+    '?first=param&ui_version='
+  );
+  t.is(
+    extractVersion('?first[]=param&first[]=param2&ui_version=hi').search,
+    '?first[]=param&first[]=param2'
+  );
 });

86e8b6adadefb646e89b3064555578391837a193
Aaron Panchal <aaron.panchal@appfolio.com>
2018-03-08 14:05:50 -0800

    add guts of UISwap

--- index.js
+++ index.js
@@ -7,4 +7,59 @@ export function extractVersion(search = '') {
   };
 }
 
-export default function UISwap({ base, devBase }) {}
+function createTag(src) {
+  let tag;
+
+  if (src.endsWith('.css')) {
+    tag = document.createElement('link');
+    tag.href = src;
+    tag.rel = 'stylesheet';
+  } else {
+    tag = document.createElement('script');
+    tag.src = src;
+  }
+
+  return tag;
+}
+
+function loadFile(src) {
+  return new Promise((resolve, reject) => {
+    const tag = createTag(src);
+
+    tag.addEventListener('load', resolve, false);
+    tag.addEventListener('error', reject, false);
+
+    document.body.append(tag);
+  });
+}
+
+export default function UISwap({
+  base,
+  devBase,
+  files,
+  defaultVersion = 'latest'
+}) {
+  let { version: newVersion, search } = extractVersion(window.location.search);
+
+  if (newVersion) {
+    const updatedURL = [
+      window.location.origin,
+      window.location.pathname,
+      search
+    ].join('');
+
+    window.history.replaceState({}, null, updatedURL);
+
+    sessionStorage.setItem('ui', newVersion);
+  }
+
+  const version = sessionStorage.getItem('ui');
+
+  if (version) {
+    files.forEach(file => loadFile(`${base}/${version}/${file}`));
+  } else if (devBase) {
+    files.forEach(file => loadFile(`${devBase}/${file}`));
+  } else {
+    files.forEach(file => loadFile(`${base}/${defaultVersion}/${file}`));
+  }
+}
